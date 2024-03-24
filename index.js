828469251f65b7746cac484f4e658ad516e90ea4
binhntps30045 <binhntps30045@fpt.edu.vn>
2024-03-25 00:43:33 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
 app.get('/tin-tuc', (req, res) => {
     var a = 1;
     var b = 4;
